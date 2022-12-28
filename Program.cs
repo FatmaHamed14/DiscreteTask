3618f50e130debd1b61fae70049ac6a0d9853943
FatmaHamed14 <121578011+FatmaHamed14@users.noreply.github.com>
2022-12-28 22:36:04 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project__1program
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            {
+                Console.WriteLine("Enter starting number:");
+                int startNum = int.Parse(Console.ReadLine());
+                Console.WriteLine("Enter ending number :");
+                int endNum = int.Parse(Console.ReadLine());
+                Console.WriteLine("the prime number are  ");
+
+
+                for (int i = startNum; i <= endNum; i++)
+                {
+                    int counter = 0;
+                    for (int j = 2; j <= i / 2; j++)
+                    {
+                        if (i % j == 0)
+                        {
+                            counter++;
+                            break;
+                        }
+                    }
+                    if (counter == 0 && i != 1)
+                    {
+                        Console.WriteLine(i);
+                    }
+                }
+            }
+
+
+        }
+   
+
+
+} 
+    }
+
+
+
+
+
+
+
+
